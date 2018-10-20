8ba3159d50156339a1d83a409218ea6301c72243
Taylor Liss <tayloraliss@gmail.com>
2018-10-19 20:26:37 -0400

    Moved logic from controller to helper

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
 
-    # https://ruby-doc.org/stdlib-2.5.1/libdoc/net/http/rdoc/Net/HTTP.html
-    # https://battlefieldtracker.com/bf1/api/Loadout/GetPresets?platform=3&displayName=samort7
-
-    query = {
-        "platform"     => 3,
-        "displayName"      => 'samort7'
-    }
-    headers = {
-        "TRN-Api-Key"  => Rails.application.credentials.trn_api_key
-    }
-
-    @result = HTTParty.post(
-        "https://battlefieldtracker.com/bf1/api/Loadout/GetPresets",
-        :query => query,
-        :headers => headers
-    ).body
+    @result = helpers.player_kit
 
   end
 
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,19 @@
 module StaticPagesHelper
+
+  def player_kit
+    query = { platform: 3, displayName: 'samort7'}
+    headers["TRN-Api-Key"] = Rails.application.credentials.trn_api_key
+
+    results = HTTParty.post(
+        "https://battlefieldtracker.com/bf1/api/Loadout/GetPresets",
+        :query => query,
+        :headers => headers
+    )
+
+    results = JSON.parse(results.body)
+
+    results['result']['kits']['0'][0]['0']
+  end
+
+
 end
