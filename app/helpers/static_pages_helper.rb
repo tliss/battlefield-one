70e6a05b212534fc3e1bda81cb11ade4a16e1192
Taylor Liss <tayloraliss@gmail.com>
2018-10-22 11:13:02 -0400

    Refactor to use correct api call to itemlist

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,7 +1,24 @@
 module StaticPagesHelper
 
   def player_kit
-    query = { platform: 3, displayName: 'samort7'}
+    player_kits = retrieve_player_kits('samort7')
+    weapon_list = create_weapon_list
+
+    readable_kit_list = []
+    player_kits['result']['kits'].each_value do |kit|
+      unless kit[0].empty?
+        readable_kit = []
+        kit[0].each_value do |weapon_id|
+          readable_kit.push(find_weapon_name(weapon_id, weapon_list))
+        end
+        readable_kit_list.push(readable_kit)
+      end
+    end
+    readable_kit_list
+  end
+
+  def retrieve_player_kits(player)
+    query = { platform: 3, displayName: player}
     headers["TRN-Api-Key"] = Rails.application.credentials.trn_api_key
 
     results = HTTParty.post(
@@ -10,53 +27,32 @@ module StaticPagesHelper
         :headers => headers
     )
 
-    results = JSON.parse(results.body)
-
-    weapon_list = create_weapon_list('samort7')
-
-    final = []
-    results['result']['kits'].each do |key, value|
-      unless value[0].empty?
-        kit = []
-        value[0].each_value do |weapon_id|
-          kit.push(find_weapon_name(weapon_id, weapon_list))
-        end
-        final.push(kit)
-      end
-    end
-
-    final
+    JSON.parse(results.body)
   end
 
-  def create_weapon_list(display_name)
-    query = { platform: 3, displayName: (display_name)}
+  def create_weapon_list
+    query = { platform: 3}
     headers["TRN-Api-Key"] = Rails.application.credentials.trn_api_key
 
     results = HTTParty.post(
-        "https://battlefieldtracker.com/bf1/api/Progression/GetWeapons",
+        "https://battlefieldtracker.com/bf1/api/Loadout/GetItems",
         :query => query,
         :headers => headers
     )
 
     results = JSON.parse(results.body)
-
     weapon_array = {}
 
-    results['result'].each do |result|
-      result['weapons'].each do |weapon|
-        name = weapon['name']
-        id = weapon['guid']
-        weapon_array[id] = name
-      end
+    results['result'].each do |item_id, value|
+      item_name = value['name']
+      weapon_array[item_id] = item_name
     end
-
     weapon_array
-
   end
 
   def find_weapon_name(weapon_id, weapon_list)
-    if weapon_list.key?(weapon_id.downcase)
-      weapon_list[weapon_id.downcase]
+    if weapon_list.key?(weapon_id)
+      weapon_list[weapon_id]
     else
       weapon_id
     end
