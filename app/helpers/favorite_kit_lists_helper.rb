3db6457a90cf5240c6f42bdb70962585c6912b2c
Taylor Liss <tayloraliss@gmail.com>
2018-11-06 17:23:13 -0500

    initial work on adding favorites

---
+++ app/helpers/favorite_kit_lists_helper.rb
@@ -0,0 +1,2 @@
+module FavoriteKitListsHelper
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :kit_list_users
   has_many :kit_lists, through: :kit_list_users
+
+  # Favorites a kit.
+  def favorite(kit_list)
+    kit_lists << kit_list
+  end
+
+  # Unfollows a user.
+  def unfavorite(kit_list)
+    kit_lists.delete(kit_list)
+  end
+
+  # Returns true if the current user is following the other user.
+  def favorited?(kit_list)
+    kit_lists.include?(kit_list)
+  end
+
 end
