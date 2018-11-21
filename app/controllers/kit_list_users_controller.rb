430d112b76b4fdde2e85e49b6634a01edc58c195
Taylor A. Liss <tayloraliss@gmail.com>
2018-11-21 16:39:18 -0500

    Sorting out favo/unfav button

--- app/controllers/kit_list_users_controller.rb
+++ app/controllers/kit_list_users_controller.rb
@@ -1,5 +1,17 @@
 class KitListUsersController < ApplicationController
     def create
         KitListUser.create(user_id: params[:user_id], kit_list_id: params[:kit_list_id])
+        respond_to do |format|
+            format.html { redirect_to @user }
+            format.js
+        end
+    end
+
+    def destroy
+        KitListUser.destroy(user_id: params[:user_id], kit_list_id: params[:kit_list_id])
+        respond_to do |format|
+            format.html { redirect_to @user }
+            format.js
+        end
     end
 end
